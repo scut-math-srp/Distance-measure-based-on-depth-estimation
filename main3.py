1eb246929e94296345266c4b87fe93d8b1ce3adb
Haochen Yang <42698149+JeffreyMagee@users.noreply.github.com>
2021-01-25 12:08:13 +0800

    主程序函数，调用init_window程序

---
+++ main3.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from init_window import Tkwindow
+
+if __name__ == "__main__":
+    # 初始化窗口
+    window = tk.Tk()
+    window.title('深度估计')
+
+    # 调用Tkwindow类 添加窗口内容
+    app_window = Tkwindow(window)
+    app_window()
+
+    window.mainloop()
